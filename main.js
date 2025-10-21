821b6ecd2fd25497aa612681d3626404494d2f6f
Priscila Oliveira Alberton <145785226+Priscilaoliveiraalberton@users.noreply.github.com>
2025-10-21 11:33:34 -0300

    21/10

--- main.js
+++ main.js
@@ -78,9 +78,22 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[atual]
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 
 }
 
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa .texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++
+            mostraPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+
+}
+
+
 mostraPergunta();
-
-
